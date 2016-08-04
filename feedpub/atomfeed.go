175b9416046b3c92ada8628e992246c40978abd1
d-smith <doug.smith.mail@gmail.com>
2016-08-04 10:38:15 -0700

    Implemented prev/next/recent feed navigation

--- feedpub/atomfeed.go
+++ feedpub/atomfeed.go
@@ -33,12 +33,34 @@ func connectToDB(connectStr string) (*sql.DB, error) {
 	return db, nil
 }
 
+func lastUpdateForFeed(feedid string) (time.Time, error) {
+	var lastUpdate time.Time
+
+	rows, err := db.Query(`select event_time from feed_data where feedid = :1 and rownum = 1 order by id desc`, feedid)
+	if err != nil {
+		return lastUpdate, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		rows.Scan(&lastUpdate)
+	}
+
+	err = rows.Err()
+
+	return lastUpdate, err
+}
+
 func feedHandler(rw http.ResponseWriter, req *http.Request) {
+
 	feedid := mux.Vars(req)["feedid"]
 	if feedid == "" {
 		http.Error(rw, "No feed id in uri", http.StatusInternalServerError)
 		return
 	}
+
+	log.Infof("processing request for feed %s", feedid)
 
 	//Look up previous
 	feedIdFromDB, previousFeed, err := lookupFeed(feedid)
@@ -52,7 +74,58 @@ func feedHandler(rw http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	rw.Write([]byte(previousFeed))
+	log.Infof("previous feed is %s", previousFeed)
+
+	next, feedIdFromDB, err := lookupNext(feedid)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	log.Infof("next feed is %s", next)
+
+	updated, err := lastUpdateForFeed(feedid)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	log.Infof("last update was %s", updated)
+
+	feed := atom.Feed{
+		Title:   "Event store feed",
+		ID:      feedid,
+		Updated: atom.TimeStr(updated.Format(time.RFC3339)),
+	}
+
+	self := atom.Link{
+		Href: fmt.Sprintf("http://localhost:4000/notifications/%s", feedid),
+		Rel:  "self",
+	}
+
+	feed.Link = append(feed.Link, self)
+
+	if previousFeed != "" {
+		feed.Link = append(feed.Link, atom.Link{
+			Href: fmt.Sprintf("http://localhost:4000/notifications/%s", previousFeed),
+			Rel:  "previous",
+		})
+	}
+
+	if next != "" {
+		feed.Link = append(feed.Link, atom.Link{
+			Href: fmt.Sprintf("http://localhost:4000/notifications/%s", next),
+			Rel:  "next",
+		})
+	}
+
+	out, err := xml.Marshal(&feed)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	rw.Write(out)
 
 }
 
@@ -77,6 +150,26 @@ func currentFeed() (string, error) {
 
 func lookupFeed(id string) (string, string, error) {
 	rows, err := db.Query(`select feedid, previous from feeds where feedid = :1`, id)
+	if err != nil {
+		return "", "", err
+	}
+
+	defer rows.Close()
+
+	var feedid string
+	var previous string
+
+	for rows.Next() {
+		rows.Scan(&feedid, &previous)
+	}
+
+	err = rows.Err()
+
+	return feedid, previous, err
+}
+
+func lookupNext(id string) (string, string, error) {
+	rows, err := db.Query(`select feedid, previous from feeds where previous = :1`, id)
 	if err != nil {
 		return "", "", err
 	}
