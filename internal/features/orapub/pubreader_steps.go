c2156c7aa8b7f8a6bfd69f5e0f3b1dc02cd5b4be
d-smith <doug.smith.mail@gmail.com>
2016-10-11 09:25:17 -0700

    Made db environment variables more generic

--- internal/features/orapub/pubreader_steps.go
+++ internal/features/orapub/pubreader_steps.go
@@ -41,29 +41,29 @@ func init() {
 	GlobalContext.BeforeAll(func() {
 		log.SetLevel(log.DebugLevel)
 		log.Info("Loading global context")
-		user = os.Getenv("FEED_DB_USER")
+		user = os.Getenv("DB_USER")
 		if user == "" {
-			configErrors = append(configErrors, "Configuration missing FEED_DB_USER env variable")
+			configErrors = append(configErrors, "Configuration missing DB_USER env variable")
 		}
 
-		password = os.Getenv("FEED_DB_PASSWORD")
+		password = os.Getenv("DB_PASSWORD")
 		if password == "" {
-			configErrors = append(configErrors, "Configuration missing FEED_DB_PASSWORD env variable")
+			configErrors = append(configErrors, "Configuration missing DB_PASSWORD env variable")
 		}
 
-		dbhost = os.Getenv("FEED_DB_HOST")
+		dbhost = os.Getenv("DB_HOST")
 		if dbhost == "" {
-			configErrors = append(configErrors, "Configuration missing FEED_DB_HOST env variable")
+			configErrors = append(configErrors, "Configuration missing DB_HOST env variable")
 		}
 
-		dbPort = os.Getenv("FEED_DB_PORT")
+		dbPort = os.Getenv("DB_PORT")
 		if dbPort == "" {
-			configErrors = append(configErrors, "Configuration missing FEED_DB_PORT env variable")
+			configErrors = append(configErrors, "Configuration missing DB_PORT env variable")
 		}
 
-		dbSvc = os.Getenv("FEED_DB_SVC")
+		dbSvc = os.Getenv("DB_SVC")
 		if dbSvc == "" {
-			configErrors = append(configErrors, "Configuration missing FEED_DB_SVC env variable")
+			configErrors = append(configErrors, "Configuration missing DB_SVC env variable")
 		}
 		log.Infof("Config errors after loading global context: %s", strings.Join(configErrors, ", "))
 	})
